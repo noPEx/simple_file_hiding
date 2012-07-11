f0a403c502763071e1bba3dafb582ca249578d3e
soumya mandi <soumyamandi@gmail.com>
2012-07-11 21:54:24 +0530

    script complete albeit a raw one but works

---
+++ eso.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+int main( int argc,char** argv )
+{
+	FILE* fp = fopen( "eso6","r" ) ;
+	//FILE* fp = fopen( "2012-07-06 18.16.55.jpg","r" ) ;
+
+	if( !fp ){
+		printf("FILE can't be opened\n" ) ;
+	}
+
+
+	printf("ting ting tiding!\n") ;
+
+	//Password verification
+	char pass[] = "5656\n" ;
+	int nbytes = 5  ;
+	char* input = ( char* )malloc( sizeof( char )*100 )  ;
+	getline( &input,&nbytes,stdin ) ;
+	//printf("input is : %sheh",input ) ;
+
+	if( strcmp( pass,input ) != 0 ){
+		//printf("match found \n" ) ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		printf("nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n") ;
+		return -1 ;
+	}
+
+	unsigned char* newf  = ( unsigned char* )malloc( sizeof( unsigned char )*1129971 ) ;
+
+	int counter = 1 ;
+	int current ;
+	current = fgetc( fp ) ; 
+	while( current != EOF ){
+		newf[ counter ] = ( unsigned char )current ;	
+		//printf("current is : %d\n", current ) ;
+		current = fgetc( fp ) ;
+		counter++ ;
+	}
+
+	fclose( fp ) ; 
+
+	counter -= 1;
+	
+	FILE* fpW = fopen( "eso.jpg","w" ) ;
+	//FILE* fpW = fopen( "eso2.jpg","w" ) ;
+
+	while( counter >0 ){
+		//printf("fwrite is : %d\n",fwrite( newf+counter,1,1,fpW )  );
+		fwrite( newf+counter,1,1,fpW ); 
+		counter-- ;
+	}
+	fclose( fpW ) ;
+
+
+
+}
